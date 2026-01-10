c2ca0f4f03a6deed0e97f03b2f6379a3a62e70e4
Vanshika <vanshikachauhan004@gmail.com>
2026-01-10 23:04:59 +0530

    fixed firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,16 @@
 const admin = require("firebase-admin");
+const { getFirestore } = require("firebase-admin/firestore");
 const serviceAccount = require("../../serviceAccountKey.json");
 
 let db;
 
 const initializeFirebase = () => {
     if (!admin.apps.length) {
-        admin.initializeApp({
+        const app = admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
         });
-
-        // 👇 CONNECT TO YOUR REAL DATABASE
-        db = admin.firestore({ databaseId: "advertise" });
+        
+        db = getFirestore(app, "advertise");
 
         console.log("Firebase Admin initialized with database: advertise");
     }
