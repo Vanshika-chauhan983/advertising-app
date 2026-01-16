74e0205a1a7a246a4f2aff8bc41445c9f34a73f3
Vanshika <vanshikachauhan004@gmail.com>
2026-01-16 11:43:33 +0530

    Wallet issue fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,25 @@
 const admin = require("firebase-admin");
-const { getFirestore } = require("firebase-admin/firestore");
 const serviceAccount = require("../../serviceAccountKey.json");
 
 let db;
 
 const initializeFirebase = () => {
     if (!admin.apps.length) {
-        const app = admin.initializeApp({
+        admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
         });
-        
-        db = getFirestore(app, "advertise");
 
-        console.log("Firebase Admin initialized with database: advertise");
+        db = admin.firestore();
+
+        console.log("Firebase Admin initialized");
     }
 };
 
-const getDb = () => db;
+const getDb = () => {
+    if (!db) {
+        console.log(" DB IS NULL");
+    }
+    return db;
+};
 
 module.exports = { admin, initializeFirebase, getDb };
