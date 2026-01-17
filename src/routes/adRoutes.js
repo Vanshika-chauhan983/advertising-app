5916b39632c06ba302a29257f538e54b788bfa00
Vanshika <vanshikachauhan004@gmail.com>
2026-01-18 03:28:20 +0530

    Fix Ad Controller

--- src/routes/adRoutes.js
+++ src/routes/adRoutes.js
@@ -7,6 +7,5 @@ const router = Router();
 router.get('/feed', verifyToken, adController.getAdFeed);
 router.post('/complete', verifyToken, adController.completeAd);
 router.post('/', verifyAdmin, adController.createAd);
-router.get('/stats', verifyAdmin, adController.getStats);
 
 module.exports = router;
