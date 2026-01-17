c422269946ac2ed4108971678c25da8dea49aaf0
Vanshika <vanshikachauhan004@gmail.com>
2026-01-18 03:36:30 +0530

    Fix wallet Routes

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
-const walletController = require('../controllers/walletController');
-const verifyToken = require('../middleware/authMiddleware');
+const { Router } = require('express')
+const walletController = require('../controllers/walletController')
+const { verifyToken } = require('../middleware/authMiddleware')
 
-const router = Router();
+const router = Router()
 
-router.get('/balance', verifyToken, walletController.getBalance);
-router.post('/redeem', verifyToken, walletController.redeemPoints);
+router.get('/balance', verifyToken, walletController.getBalance)
+router.post('/redeem', verifyToken, walletController.redeemPoints)
 
-module.exports = router;
+module.exports = router
